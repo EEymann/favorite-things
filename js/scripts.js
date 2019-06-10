f2d4e92f8ac4ada5a3134d97a390e9201bea5f27
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2019-06-09 22:22:35 -0700

    write code in js to display user input into arrays.

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  $("form#survey").submit(function(event){
+    event.preventDefault();
+    var music= $("#music").val();
+    var color= $("#color").val();
+    var food= $("#food").val();
+    var dessert= $("#dessert").val();
+    var animal= $("#animal").val();
+
+    var favorites1 = [music, color, food, dessert, animal]; //gathers each form input value and places them into one array.
+
+    var favorites2 = favorites1.slice(0,2); //music, color
+    var favorites3 = favorites2.reverse(); //color, music
+    var favorites4 = favorites3.concat(favorites1[2]); // builds a new array out of only the second, first and third elements of the first array, in that order.
+
+    var newArray = favorites4.push(favorites1[0],favorites1[1],favorites1[2],favorites1[3],favorites1[4]); //use square bracket notation to access each element of the original array, and use the push() method to add these elements to the new array.
+
+    $(".listOutput").show();
+    $("#output0").text(favorites4[0]);
+    $("#output1").text(favorites4[1]);
+    $("#output2").text(favorites4[2]);
+    $("#output3").text(favorites4[3]);
+    $("#output4").text(favorites4[4]);
+    $("#output5").text(favorites4[5]);
+    $("#output6").text(favorites4[6]);
+    $("#output7").text(favorites4[7]);
+    //display the new array for the user in your page's HTML as list items in a <ul> tag.
+
+  });
+});
